5f5eaefa5178c1ac3f1bdbe492a6a3e31ef20714
trialmysite <visitjslabs@gmail.com>
2025-08-11 20:42:04 +0530

    Updated vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,37 @@
 /// <reference types="vite/client" />
+
+// MIDI API types
+declare global {
+  interface Navigator {
+    requestMIDIAccess(): Promise<WebMidi.MIDIAccess>
+  }
+}
+
+declare namespace WebMidi {
+  interface MIDIAccess {
+    inputs: MIDIInputMap
+    outputs: MIDIOutputMap
+  }
+
+  interface MIDIInputMap extends Map<string, MIDIInput> {
+    forEach(callback: (input: MIDIInput) => void): void
+  }
+
+  interface MIDIOutputMap extends Map<string, MIDIOutput> {
+    forEach(callback: (output: MIDIOutput) => void): void
+  }
+
+  interface MIDIInput {
+    onmidimessage: ((event: MIDIMessageEvent) => void) | null
+  }
+
+  interface MIDIOutput {
+    send(data: Uint8Array): void
+  }
+
+  interface MIDIMessageEvent {
+    data: Uint8Array
+  }
+}
+
+export {}
